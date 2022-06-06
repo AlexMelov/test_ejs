4ebe4da35d6e7afe9ea3a282f5e262f8e884caeb
Aleksandar <melov.aleksandar2@gmail.com>
2022-06-06 17:49:23 +0200

    add css

--- public/script/functions.js
+++ public/script/functions.js
@@ -2,6 +2,7 @@ document.addEventListener("init", function (event) {
     submitHandler();
 });
 
+const button = getElementById("submitButton");
 export function submitHandler() {
     console.log("working");
 }
