45773bdabcb7a8ab42ce51a8d021ffaa677e7f09
AlexMelov <melov.aleksandar2@gmail.com>
2022-06-08 16:17:03 +0200

    sending data succesfuly

--- src/routes/second-page-route.js
+++ src/routes/second-page-route.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/second-page', (req, res) => {
-  res.render('pages/second-page')
+  res.render('pages/second-page');
 });
-router.post('/second-page', (req, res) => {
+router.post('/second-page', (req) => {
   console.log(req.body);
 });
 
