79b052930f98ac937a039476a213da4ee227d2dd
Aleksandar <melov.aleksandar2@gmail.com>
2022-06-09 09:28:45 +0200

    add mongo schema

--- src/routes/second-page-route.js
+++ src/routes/second-page-route.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
+const mongoModel = require('../schema/mongo.schema');
 
 router.get('/second-page', (req, res) => {
   res.render('pages/second-page');
---
+++ src/schema/mongo.schema.js
@@ -0,0 +1,15 @@
+const {Schema} = require("mongoose");
+const mongoose = require("mongoose");
+
+const userSchema = new Schema({
+    username : {
+        type: String,
+        required : true
+    },
+    password: {
+        type: String,
+        required : true
+    }
+})
+
+module.exports = mongoose.model('Authentication', userSchema)
