31e1847fdc7b6d8de1c6017c056197344ea32af5
Aleksandar <melov.aleksandar2@gmail.com>
2022-06-09 11:51:29 +0200

    add mongo schema

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { response } = require('express');
 const router = require('./routes/home-page-route');
 const router2 = require('./routes/second-page-route');
 const router3 = require('./routes/third-page-route');
@@ -15,5 +17,9 @@ app.use('/', router);
 app.use('/', router2);
 app.use('/', router3);
 app.use(express.static(`${__dirname}/public`));
+
+mongoose.connect(process.env.DB_URL)
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch((error) => response.json({ message: error }));
 
 app.listen(8000);
--- src/routes/second-page-route.js
+++ src/routes/second-page-route.js
@@ -1,12 +1,15 @@
 const express = require('express');
+
 const router = express.Router();
-const mongoModel = require('../schema/mongo.schema');
+const MongoModel = require('../schema/mongo.schema');
 
 router.get('/second-page', (req, res) => {
   res.render('pages/second-page');
 });
-router.post('/second-page', (req) => {
-  console.log(req.body);
+router.post('/second-page', (req, res) => {
+  const auth = new MongoModel(req.body);
+  auth.save().then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
 });
 
 module.exports = router;
